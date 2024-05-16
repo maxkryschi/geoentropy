d05aec2d6af5b96872692929468b7e697c69af34
Maximilian Kryschi <maxkryschi@gmail.com>
2024-05-16 19:53:40 +0200

    README.md updated.

--- setup.py
+++ setup.py
@@ -4,12 +4,14 @@ setup(
     name='GeoEntropy',
     version='0.1.0',
     packages=find_packages(),
+    install_requires=['numpy', 'pandas', 'matplotlib', 'scipy', 'shapely'],
+    include_package_data=True,
     description='A Python package for computing geometric/spatial entropy metrics for data in matrix format.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/maxkryschi/geoentropy',
     author='Max Kryschi',
-    author_email='mail@maxkryschi.com',
+    author_email='',
     license='MIT License',
     classifiers=[
         'Programming Language :: Python :: 3',
@@ -17,6 +19,4 @@ setup(
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.9',
-    install_requires=['numpy', 'pandas', 'matplotlib', 'scipy', 'shapely'
-                      ],
 )
