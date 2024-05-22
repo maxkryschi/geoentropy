51085aab05343d51eb5bec8adc9d7b5ec9f1b99a
Maximilian Kryschi <maxkryschi@gmail.com>
2024-05-22 13:53:55 +0200

    README.md updated and improved.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='GeoEntropy',
-    version='0.1.1',
+    version='0.2.0',
     packages=find_packages(),
     install_requires=['numpy', 'pandas', 'matplotlib', 'scipy', 'shapely'],
     include_package_data=True,
